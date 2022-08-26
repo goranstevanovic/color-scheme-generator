9ba58a6b28aa3dbe4d5c5258a9b8667df4095742
Goran Stevanovic <stevanovicgoran@proton.me>
2022-08-26 13:44:32 +0200

    Copy color code to clipboard when clicked
    See #6

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const seedColorInput = document.getElementById('seed-color');
 const colorSchemeInput = document.getElementById('color-scheme');
 const colorQtyInput = document.getElementById('color-qty');
 const getColorSchemeBtn = document.getElementById('get-color-scheme');
+const colorListEl = document.getElementById('color-list');
 
 async function getColorScheme(seedColor, colorScheme, colorQty) {
   const res = await fetch(
@@ -14,16 +15,14 @@ async function getColorScheme(seedColor, colorScheme, colorQty) {
 }
 
 function displayColorScheme(colors) {
-  const colorListEl = document.getElementById('color-list');
-
   const colorsEl = colors.map((color) => {
     return `
       <div class="color">
-      <div class="color-swatch" style="background-color: ${color.hex.value};"></div>
+      <div class="color-swatch" data-color-code="${color.hex.value}" style="background-color: ${color.hex.value};"></div>
       <div class="color-info">
-        <p class="color-code">${color.hex.value}</p>
-        <p class="color-code">${color.rgb.value}</p>
-        <p class="color-code">${color.hsl.value}</p>
+        <p class="color-code" data-color-code="${color.hex.value}">${color.hex.value}</p>
+        <p class="color-code" data-color-code="${color.rgb.value}">${color.rgb.value}</p>
+        <p class="color-code" data-color-code="${color.hsl.value}">${color.hsl.value}</p>
       </div>
     </div>
     `;
@@ -49,4 +48,47 @@ async function handleGetColorScheme(e) {
   displayColorScheme(colors);
 }
 
+function displayToastMessage(message) {
+  let toastEl = null;
+
+  if (document.getElementById('toast-message')) {
+    toastEl = document.getElementById('toast-message');
+    toastEl.remove();
+  }
+
+  toastEl = document.createElement('p');
+  toastEl.id = 'toast-message';
+  toastEl.classList.add('toast-message');
+  toastEl.innerHTML = message;
+
+  document.body.appendChild(toastEl);
+
+  setTimeout(function () {
+    toastEl.remove();
+  }, 3000);
+}
+
+function handleColorCodeClick(e) {
+  if (
+    !e.target.classList.contains('color-swatch') &&
+    !e.target.classList.contains('color-code')
+  ) {
+    console.log('not target');
+    return;
+  }
+
+  const colorCode = e.target.dataset.colorCode;
+
+  navigator.clipboard
+    .writeText(colorCode)
+    .then(function () {
+      displayToastMessage(`Color code <b>${colorCode}</b> copied to clipboard`);
+    })
+    .catch(function (error) {
+      displayToastMessage('There was an error when copying to clipboard');
+    });
+}
+
 getColorSchemeBtn.addEventListener('click', handleGetColorScheme);
+
+colorListEl.addEventListener('click', handleColorCodeClick);
